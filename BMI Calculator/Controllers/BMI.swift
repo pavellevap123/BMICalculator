0ae1e2bb234f8d42fc5d7e555d171b6bbec05826
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2021-07-17 20:49:37 +0300

    Complete project.

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Pavel Paddubotski on 11.07.21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
